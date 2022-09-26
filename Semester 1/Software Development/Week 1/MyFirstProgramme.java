81b730665b75bc26718ddd232c908a99c5459e4a
DenisWAD <denismurray2@gmail.com>
2022-09-26 20:37:38 +0100

    First commit for Semester repo

---
+++ Semester 1/Software Development/Week 1/MyFirstProgramme.java
@@ -0,0 +1,16 @@
+/*
+MyFirstProgramme.java
+Denis Murray
+26/09/22
+*/
+
+public class MyFirstProgramme {
+    public static void main(String [] args) {
+
+        System.out.println("Hi there");
+
+
+    }
+
+
+}
