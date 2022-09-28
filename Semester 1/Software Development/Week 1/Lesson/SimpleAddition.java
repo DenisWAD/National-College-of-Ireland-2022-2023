d03402dfe032dd9c144fa1c235b6b9e11f2bcebd
DenisWAD <denismurray2@gmail.com>
2022-09-28 18:27:58 +0100

    Lesson 2

---
+++ Semester 1/Software Development/Week 1/Lesson/SimpleAddition.java
@@ -0,0 +1,28 @@
+/*
+public class Fixme{
+	public static void main(String[] args){
+		//Fill in the gaps in code by replacing the **** sections
+		//There may be errors other than just blanks, but start with blanks first
+
+		//addition
+		int x=7;
+		int y=8;
+		int ans=x*y;
+		//print
+		System.out.println(ans);
+	}
+}
+*/
+
+// Code Challenge - Simple Addition
+public class SimpleAddition{
+	public static void main(String[] args){
+		/*Fill in the gaps in code by replacing the **** sections*/
+		//addition
+		int x=7;
+		int y=8;
+		int ans=x+y;
+		//print
+		System.out.println(ans);
+	}
+}
